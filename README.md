6450f8c0d7eb2b6a050e850c98d5d07b1d4fb0f1
JohnnyWobble <tonald.drump2.0@gmail.com>
2021-01-14 14:02:33 -0500

    Preparing for twilio

---
+++ README.md
@@ -0,0 +1,2 @@
+# To install pyaudio on Ubuntu
+sudo apt-get install python3-pyaudio
--- core/ai.py
+++ core/ai.py
@@ -1,14 +1,9 @@
-from warnings import filterwarnings
 import datetime
 
 from fastai.text.all import *
-# from fastai import text
-# import fastai
+from fastai.callback.all import *
 
-model: TextLearner = load_learner('models/rev4-1.model')
-print(TextLearner)
-
-# filterwarnings('ignore')
+model: TextLearner = load_learner('models/rev5-1.model')
 
 
 class Prediction:
@@ -22,8 +17,7 @@ class Prediction:
         return f"Prediction: {self.name}"
 
 
-CATEGORIES = ["Question", "Command", "General Exchange", "Persuasion", "Information", "Information",
-              "Conditional"]
+CATEGORIES = ["Question", "Command", "General Exchange", "Persuasion", "Information", "Conditional"]
 PREDICTIONS = {c: Prediction(c) for c in CATEGORIES}
 
 
@@ -38,6 +32,5 @@ if __name__ == '__main__':
         user_input = input("> ")
         now = datetime.now()
         prediction = predict_text(user_input)
-        time_seconds = (now - datetime.now()).seconds
-        print(prediction, end=' ')
-        print(time_seconds, "s", sep="")
+        time_seconds = (datetime.now() - now).microseconds / 1000000
+        print(f"{prediction} {time_seconds}s")
